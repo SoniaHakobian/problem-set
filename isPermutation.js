386da6b9a00ffacef4f23b79b2cd9c29f84874fe
Sonia Hakobian <sonyahakobyan97@gmail.com>
2017-12-20 21:13:26 +0400

    Update isPermutation.js

--- isPermutation.js
+++ isPermutation.js
@@ -7,3 +7,38 @@ function isPermutation(a, b) {
     return a.split("").sort().join() === b.split("").sort().join();
 }
 isPermutation('abc', 'bca');
+
+--------------------------------------------------------------------------------------------------
+// Long solution. 
+//Time complexity: O(n)
+function isPermutation(a, b) {
+    if (a.length !== b.length) {
+        return false;
+    }
+
+    var letters = {},
+        length = a.length;
+
+    for (var x = 0; x < length; x++) {
+        var l = a.charAt(x);
+        letters[l] = (isNaN(letters[l]) ? 1 : letters[l] + 1);
+    }
+
+    for (var y = 0; y < length; y++) {
+        var m = b.charAt(y);
+        letters[m]--;
+    }
+
+    for (var z in letters) {
+        if (letters[z] !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+isPermutation("dog", "god"); // true
+isPermutation("dog", "gdo"); // true
+isPermutation("dog", "odg"); // true
+isPermutation("dog", "ogg"); // false
