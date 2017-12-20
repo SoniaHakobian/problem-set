87566ff609e652d20a233f828597a379d8d9d0ff
Sonia Hakobian <sonyahakobyan97@gmail.com>
2017-12-20 21:05:03 +0400

    Create isPermutation.js

---
+++ isPermutation.js
@@ -0,0 +1,9 @@
+//Given two strings,write a method to decide if one is a permutation of the other.
+
+function isPermutation(a, b) {
+    if (a.length !== b.length) 
+        return false;
+
+    return a.split("").sort().join() === b.split("").sort().join();
+}
+isPermutation('abc', 'bca');
